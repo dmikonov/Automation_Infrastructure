767d6e64fcf39e4f6d58ce5b68325b6caaa8046d
dmitrii.nikonov <dmikonov@yahoo.com>
2025-03-22 15:27:42 +0200

    New version

--- src/test/java/sanity/CalculatorDesktop.java
+++ src/test/java/sanity/CalculatorDesktop.java
@@ -2,10 +2,12 @@ package sanity;
 
 import extensions.Verifications;
 import io.qameta.allure.Description;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import utilities.CommonOps;
 import workflows.DesktopFlows;
 
+@Listeners(utilities.Listeners.class)
 public class CalculatorDesktop extends CommonOps {
     @Test(description = "Test01 - Verify Addition Command")
     @Description("This test adds new task and verify it in list")
